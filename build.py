180f81916b25386f9404c57eaabd078177d7d4d9
andrew-up <aageew@bk.ru>
2023-02-12 22:01:24 +0300

    система обновлений

--- build.py
+++ build.py
@@ -3,8 +3,14 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'install_update.py', '-F'
 ])
+
 print('======================')
+
 PyInstaller.__main__.run([
-    'main.spec',
-    '--noconfirm'
+    'main.py',
+    '--noconfirm',
+    '--add-data=project.xml;.',
+    '--add-data=Image_test/;Image_test',
+    '--add-data=unet_model/;unet_model',
+    '--add-data=dist/install_update.exe;.',
 ])
